cca316c88a4025ffd63dbda0a687ebcafb9c96b3
leoseis <eboscrib2@yahoo.com>
2023-08-16 16:27:23 +0100

    'second'

--- market/market/urls.py
+++ market/market/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf. urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -6,6 +8,5 @@ from core.views import index, contact
 urlpatterns = [
     path('', index, name = 'index'),
     path('contact/', contact, name = 'contact'),
-    path('admin/', admin.site.urls),
-    
-]
+    path('admin/', admin.site.urls),  
+ ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
