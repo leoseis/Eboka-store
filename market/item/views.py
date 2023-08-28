8a5b0591c6cf4c5869a09e572003888f0393bf69
leoseis <eboscrib2@yahoo.com>
2023-08-28 12:27:24 +0100

    'detail'

--- market/item/views.py
+++ market/item/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from .models import item
 
-# Create your views here.
+def detail (request ,pk):
+    item = get_object_or_404(item, pk=pk)
+
+    return render(request, 'item/detail.html',{
+        'item': item
+    })
+    
