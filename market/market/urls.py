77e2169910745700dbe3509f6e199d5fe5e7ccb9
leoseis <eboscrib2@yahoo.com>
2023-08-28 11:38:17 +0100

    'count'

--- market/market/urls.py
+++ market/market/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from core.views import index, contact
 
 urlpatterns = [
-    path('', index, name = 'index'),
-    path('contact/', contact, name = 'contact'),
-    path('admin/', admin.site.urls),  
- ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
+   path('', index, name ='index'),
+   path('contact/' , contact, name='contact') ,
+   path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
