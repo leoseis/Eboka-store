a6903cbd9be21ba70a481fcc33b9963bcbdc7223
leoseis <eboscrib2@yahoo.com>
2023-08-31 15:04:33 +0100

    'detail'

--- market/item/views.py
+++ market/item/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render , get_object_or_404
-from .models import item
+from django.shortcuts import render, get_object_or_404
 
-def detail (request ,pk):
-    item = get_object_or_404(item, pk=pk)
+from .models import Item
+
+def detail (request, pk):
+    item = get_object_or_404(Item, pk=pk)
 
     return render(request, 'item/detail.html',{
         'item': item
     })
-    
+       
