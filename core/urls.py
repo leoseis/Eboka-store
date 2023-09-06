8e3f095aa1d918d4649223e825fd9ecee4fb19a9
leoseis <eboscrib2@yahoo.com>
2023-09-06 11:33:30 +0100

    'core'

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views                  #importing all views belonging to this app
+
+app_name = 'core'
+
+urlpatterns =[
+    path ('', views.index, name = 'index'),
+    path('contact/' , views.contact, name='contact') ,
+]
