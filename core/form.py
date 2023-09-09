bb2af9642f05b77751e4921cf5beeff1b0f8c5ff
leoseis <eboscrib2@yahoo.com>
2023-09-09 06:32:24 +0100

    'anendments'

--- core/form.py
+++ core/form.py
@@ -7,3 +7,22 @@ class SignupForm(UserCreationForm):
         model = User
         fields = ('username','email', 'password1','password2' )
 
+username = forms.CharField(widget=forms.TextInput(attrs={
+     'placeholder': 'Your username',
+     'class': 'w-full py-4 px-6 rounded-xl'
+}))
+
+email = forms.CharField(widget=forms.EmailInput(attrs={
+     'placeholder': 'Your email address',
+     'class': 'w-full py-4 px-6 rounded-xl'
+}))
+
+password1= forms.CharField(widget=forms.PasswordInput(attrs={
+     'placeholder': 'password',
+     'class': 'w-full py-4 px-6 rounded-xl'
+}))
+
+password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+     'placeholder': 'Repeat password',
+     'class': 'w-full py-4 px-6 rounded-xl'
+}))
