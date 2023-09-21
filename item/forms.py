06a0de5683ee5681e29e26cfac782bd735244b4a
leoseis <eboscrib2@yahoo.com>
2023-09-21 18:52:05 +0100

    'adding'

--- item/forms.py
+++ item/forms.py
@@ -1,8 +1,33 @@
 from django import forms
 
-from .models import Item
+from .models import  Item
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
 
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('category', 'name', 'description', 'price','image',)
+        widget ={
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES
+            }),
+
+             'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+             'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+             'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+             'Image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+        }
+
+
+
+
+        
