0082e24ea542524cd9f5fdebd5a71c9cb4cd8f13
leoseis <eboscrib2@yahoo.com>
2023-09-25 11:00:41 +0100

    'delete'

--- item/views.py
+++ item/views.py
@@ -35,6 +35,7 @@ def new (request):
 
 @login_required
 def delete (request, pk):
-   item = get_object_or_404(item, pk=pk, created_by= request.user)
+   item = get_object_or_404(Item, pk=pk, created_by= request.user)
    item.delete()
-   return redirect('dashboard: index')
+   
+   return redirect('dashboard:index')
