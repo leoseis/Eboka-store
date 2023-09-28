5e6621c1ffcfb901423c0dc4f221951f2e1ebebf
leoseis <eboscrib2@yahoo.com>
2023-09-28 11:31:43 +0100

    'searchbar'

--- item/views.py
+++ item/views.py
@@ -4,6 +4,13 @@ from .forms import NewItemForm, EditItemForm
 
 from .models import Item
 
+def items (request ):
+   item = Item.objects.filter(is_sold=False)
+
+   return render (request, 'item/items.html',{
+      'items':items,
+   })
+
 def detail (request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
